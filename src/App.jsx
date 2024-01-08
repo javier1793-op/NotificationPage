b4fd8186bc2c550c4ae991bbbca57cd454c029cf
NicolasAyal <javier17utn@gmail.com>
2024-01-08 12:23:07 -0300

    notification page init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Notification from './Components/Notification'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+       <Notification/>
+      </main>
+    </>
+  )
+}
+
+export default App
